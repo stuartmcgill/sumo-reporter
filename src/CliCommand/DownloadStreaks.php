7dbc710517cd8b10cde7a0be8670ae7a051ea3a1
Stuart McGill <stuartmcgill@capuk.org>
2023-03-30 11:37:04 +0100

    Add formatting to command

--- src/CliCommand/DownloadStreaks.php
+++ src/CliCommand/DownloadStreaks.php
@@ -9,17 +9,26 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name: 'report:streaks')]
+#[AsCommand(
+    name: 'report:streaks',
+    description: 'Displays winning and losing streaks.'
+)]
 class DownloadStreaks extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Downloading wrestler streaks...');
+
         $wrestlers = (new StreakDownloader())->download();
 
         foreach ($wrestlers as $wrestler) {
             $output->writeln($wrestler->name);
         }
+
+        $io->success('Successfully completed');
 
         return Command::SUCCESS;
     }
