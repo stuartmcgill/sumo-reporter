781c798f49623b36d5a3e85a3318805d2409254f
Stuart McGill <stuartmcgill@capuk.org>
2023-04-05 19:32:24 +0100

    Add Basho test

---
+++ tests/unit/Model/BashoTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unit\Model;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use StuartMcGill\SumoScraper\Model\Basho;
+use StuartMcGill\SumoScraper\Model\Performance;
+use StuartMcGill\SumoScraper\Model\Streak;
+
+class BashoTest extends TestCase
+{
+    /** @test */
+    public function compileStreaks(): void
+    {
+        $streaks = [
+            Mockery::mock(Streak::class),
+            Mockery::mock(Streak::class),
+        ];
+
+        $performances = array_map(
+            callback: static function (Streak $streak): Performance {
+                $performance = Mockery::mock(Performance::class);
+                $performance->expects('calculateStreak')->andReturn($streak);
+
+                return $performance;
+            },
+            array: $streaks,
+        );
+
+        $basho = new Basho(1, 1, $performances);
+        $this->assertSame(expected: $streaks, actual: $basho->compileStreaks());
+    }
+}
